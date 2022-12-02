1c0b1bd9324f1807afc7e27303918e90f3635f0d
Jamie <seabirdshanty@users.noreply.github.com>
2022-12-02 09:08:35 -0800

    Create header.php

---
+++ fan/files/header.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>eternal anime v6: morning glories and you</title>
+
+	<!-- base href="https://eternal-anime.org" / -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Begin SEO meta tags -->
+	<meta name="title" content="Eternal Anime">
+	<meta name="description" content="A site dedicated to the archival of anime fanworks, as well as a collection of anime fanlistings, shrines, and tribute pages">
+	<meta name="keywords" content="eternal, anime, fanlisting, fanshrine, archive, sailor moon, webcore">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="7 days">
+	<meta name="author" content="Slam Jams">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://solstice.party/">
+    <meta property="og:title" content="eternal anime v6: morning glories and you">
+    <meta property="og:description" content="A site dedicated to the archival of anime fanworks, as well as a collection of anime fanlistings, shrines, and tribute pages">
+    <meta property="og:image" content="https://solstice.party/files/img/favicon_128.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://solstice.party/">
+    <meta property="twitter:title" content="eternal anime v6: morning glories and you">
+    <meta property="twitter:description" content="A site dedicated to the archival of anime fanworks, as well as a collection of anime fanlistings, shrines, and tribute pages">
+    <meta property="twitter:image" content="https://eternal-anime.org/files/img/favicon_128.png">
+
+	<link rel="icon" type="image/x-icon" href="https://solstice.party/files/favicon.ico">
+
+	<!-- End SEO meta tags -->
+  
+	 <!-- Google Fonts -->
+	 <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	 <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+	 <link href="https://fonts.googleapis.com/css?family=Gentium+Basic" rel="stylesheet">
+
+    <!-- bootstrap requirements -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!-- // end requirements -->
+    <link rel="stylesheet" href="./files/css/style.css" />
+    <link rel="stylesheet" href="./files/css/bubbles.css" />
+
+	<!-- for UberGallery -->
+    <script type="text/javascript" src="./resources/colorbox/jquery.colorbox.js"></script>
+	<script type="text/javascript">
+    $(document).ready(function(){
+        $("a[rel='colorbox']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
+    });
+	</script>
+
+
+  </head>
+  <body>
+    <div id="wrapper">
+
+      <div class="masthead">
+        <div id="bubble-wrap">
+          <div class="bubble x1"></div>
+          <div class="bubble x2"></div>
+          <div class="bubble x3"></div>
+          <div class="bubble x4"></div>
+          <div class="bubble x5"></div>
+          <div class="bubble x6"></div>
+          <div class="bubble x7"></div>
+          <div class="bubble x8"></div>
+          <div class="bubble x9"></div>
+          <div class="bubble x10"></div>
+        </div>
+      </div>
+	  
+	  <nav class="navbar navbar-expand-sm navbar-light bg-faded">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Brand -->
+        <a class="navbar-brand" href="index.php">eternal anime</a>
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="nav-content">
+          <ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+			  <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="Preview" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                About
+              </a>
+              <div class="dropdown-menu" aria-labelledby="Preview">
+				<a class="dropdown-item" href="index.php?about">the Host</a>
+				<a class="dropdown-item" href="index.php?history">the Domain</a>
+        <a class="dropdown-item" href="index.php?etan">the Past</a>
+              </div>
+            </li>
+		    <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="Preview" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Listings
+              </a>
+              <div class="dropdown-menu" aria-labelledby="Preview">
+				<a class="dropdown-item" href="joined.php?cat=all">Joined</a>
+				<a class="dropdown-item" href="owned.php">Owned</a>
+				<a class="dropdown-item" href="index.php?wishlist" >Wishlist</a>
+              </div>
+            </li>
+		    <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="Preview" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Archives
+              </a>
+              <div class="dropdown-menu" aria-labelledby="Preview">
+				<a class="dropdown-item" href="index.php?archives">Sites</a>
+				<a class="dropdown-item" href="index.php?extra">Extras</a>
+              </div>
+            </li>
+			 <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="Preview" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                GFX
+              </a>
+              <div class="dropdown-menu" aria-labelledby="Preview">
+				<a class="dropdown-item" href="gallery.php">Galleries</a>
+				<a class="dropdown-item" href="icons.php">Icons</a>
+				<a class="dropdown-item" href="toybox.php">Pixels</a>
+				<a class="dropdown-item" href="https://seabirdshanty.github.io/webdesigns/">Layouts</a>
+				<a class="dropdown-item disabled" href="#">Banners</a>
+              </div>
+			 </li>
+			  <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="Preview" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Writing
+              </a>
+              <div class="dropdown-menu" aria-labelledby="Preview">
+				<a class="dropdown-item" href="ao3.php">Fanfiction</a>
+				<a class="dropdown-item" href="http://github.com/seabirdshanty">Code</a>
+				<a class="dropdown-item disabled" href="#" >Essays</a>
+				<a class="dropdown-item disabled" href="#" >Anime Trivia</a>
+				<a class="dropdown-item disabled" href="#">Tutorials</a>
+				
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- BEGIN CONTENT -->
